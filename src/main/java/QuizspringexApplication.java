b2836b9f50e6f61638ca6a0034f1d0aea8b84f7a
Kleppa <jarandwaagekleppa@gmail.com>
2018-02-27 15:38:42 +0100

    upload pro

---
+++ src/main/java/QuizspringexApplication.java
@@ -0,0 +1,13 @@
+package no.jarand.quizspringex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizspringexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizspringexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/jarand/quizspringex/LocalApplicationRunner.java
@@ -0,0 +1,13 @@
+
+
+
+
+import no.jarand.quizspringex.QuizspringexApplication;
+import org.springframework.boot.SpringApplication;
+
+public class LocalApplicationRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizspringexApplication.class);
+	}
+}
